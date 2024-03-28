733ac95ea88c235549ab230ca00e80a7e8aa0415
Nwigberi Moses <70588071+TheMoeEntity@users.noreply.github.com>
2024-03-28 16:13:36 +0100

    "Add portfolio readme with detailed instructions, technologies used and features"

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        'xs': '280px',
+        'xm': '375px',
+        'xm2': '390px',
+      },
+    },
+  },
+  plugins: [],
+}
+
