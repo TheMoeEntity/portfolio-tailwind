18ab20fa5b61bdce4264e0417fa480fa26dfa90f
Nwigberi Moses <70588071+TheMoeEntity@users.noreply.github.com>
2024-05-15 19:20:06 +0100

    Update Loader component with new loading animation and styles
    - Replace previous loading animation with a new one using CSS bars
    - Add new CSS file for loader styles
    - Update background color and opacity of the loader overlay
    - Maint

--- components/Loader/index.tsx
+++ components/Loader/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
+import './loader.css'
 
 export default function Loader() {
     const [show, setShow] = useState(true);
@@ -12,8 +13,27 @@ export default function Loader() {
     if (!show) return null;
 
     return (
-        <div className="bg-[#282C34] fixed top-0 left-0 w-full h-full opacity-[0.98] flex items-center justify-center z-[200]">
-            <div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4"></div>
+        <div className="bg-[#282C34] body fixed top-0 left-0 w-full h-full opacity-[0.98] flex items-center justify-center z-[200]">
+            <div className="content">
+                <div className="bars">
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                </div>
+                <div className="bars">
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                </div>
+            </div>
         </div>
     );
 }
